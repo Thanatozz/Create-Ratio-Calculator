5291cc8beff132a5bcc59ec922c3aaf5d6b2636c
MJM_Dev <65843088+Thanatozz@users.noreply.github.com>
2026-06-12 20:51:52 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES === "true" ? "/create-ratio-calculator/" : "/",
+  base: process.env.GITHUB_PAGES === "true" ? "/Create-Ratio-Calculator/" : "/",
   test: {
     environment: "jsdom",
     globals: true,
